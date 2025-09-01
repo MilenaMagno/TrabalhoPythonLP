51ff00093fde3a7d248005f51dfb3c7c09785233
MilenaMagno <milenamagnotrg@gmail.com>
2025-08-31 21:43:27 -0300

    SQLite3 implementation for Score using proxy design pattern

--- Code/Player.py
+++ Code/Player.py
@@ -3,7 +3,7 @@
 import pygame.key
 
 from Code.Const import ENTITY_SPEED, WIN_WIDTH, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, \
-    PLAYER_KEY_RIGHT, PLAYER_KEY_SHOOT, ENTITY_SHOT_DELAY
+    PLAYER_KEY_RIGHT, PLAYER_KEY_SHOOT, ENTITY_SHOT_DELAY, WIN_HEIGHT
 from Code.Entity import Entity
 from Code.PlayerShot import PlayerShot
 
@@ -14,17 +14,16 @@ class Player(Entity):
         self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
 
-    def move(self, ):
+    def move(self):
         pressed_key = pygame.key.get_pressed()
         if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0:
             self.rect.centery -= ENTITY_SPEED[self.name]
-        if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_WIDTH:
+        if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT:
             self.rect.centery += ENTITY_SPEED[self.name]
         if pressed_key[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0:
             self.rect.centerx -= ENTITY_SPEED[self.name]
         if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH:
             self.rect.centerx += ENTITY_SPEED[self.name]
-
         pass
 
     def shoot(self):
